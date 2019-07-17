f508b22bf70c9ac981633556c8f9c237694b841e
cqwang <wangchaoqun@uqpay.com>
2019-07-17 18:17:41 +0800

    first commit

---
+++ THostUIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  THostUIDemo
+//
+//  Created by uqpay on 2019/7/17.
+//  Copyright © 2019 优钱付. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
